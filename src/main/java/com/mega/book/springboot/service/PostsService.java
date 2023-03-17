38d8339162f2c38d93b91c855653baa4597a445b
222000zzz <dragonmin25@gmail.com>
2023-03-17 13:52:15 +0900

    1.글 관련 기능 구현 2.templates mustache 세팅 3.Auditing 기능 구현

--- src/main/java/com/mega/book/springboot/service/PostsService.java
+++ src/main/java/com/mega/book/springboot/service/PostsService.java
@@ -1,6 +1,8 @@
 package com.mega.book.springboot.service;
 
+import com.mega.book.springboot.domain.posts.Posts;
 import com.mega.book.springboot.domain.posts.PostsRepository;
+import com.mega.book.springboot.web.dto.PostsResponseDto;
 import com.mega.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    @Transactional
+    public Long update(Long id, PostsSaveRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+        return  new PostsResponseDto(entity);
     }
 }
